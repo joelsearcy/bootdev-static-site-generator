1baff460f354a44c78bf8e334103d5d6441c1d12
joelsearcy <searcy.joel@gmail.com>
2025-04-03 15:50:44 -0600

    Fix underscore italics parsing

--- src/textnodeparser.py
+++ src/textnodeparser.py
@@ -67,6 +67,7 @@ def text_to_textnodes(text):
     nodes = [TextNode(text, TextType.TEXT)]
     nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
     nodes = split_nodes_delimiter(nodes, "*", TextType.ITALIC)
+    nodes = split_nodes_delimiter(nodes, "_", TextType.ITALIC)
     nodes = split_nodes_delimiter(nodes, "`", TextType.CODE)
     nodes = split_nodes_images(nodes)
     nodes = split_nodes_links(nodes)
